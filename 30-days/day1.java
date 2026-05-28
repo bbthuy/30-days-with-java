abb3949a54a3f643cd0dbeae3b764dba5bd383da
Bùi Bích Thủy <bichthuy181205@gmail.com>
2026-05-28 14:47:07 +0700

    lam quen

---
+++ 30-days/day1.java
@@ -0,0 +1,12 @@
+void main (){
+    System.out.println("Hello World");
+    System.out.println("GIỚI THIỆU VỀ jshell");
+    System.out.println("jshell hoạt động theo cơ chế REPL: Read-Eval-Print-Loop");
+    System.out.println("Một số lệnh điều khiển hay dùng:");
+    System.out.println("/list: Xem lại tất cả các đoạn code (snippets) bạn đã gõ từ đầu phiên.");
+    System.out.println("/vars, /methods: Xem danh sách các biến hoặc hàm đã định nghĩa.");
+    System.out.println("/open: Tải các đoạn code từ một file có sẵn vào JShell.");
+    System.out.println("/save: Lưu toàn bộ phiên làm việc ra một file text.");
+    System.out.println("/exit: Thoát JShell.");
+    
+}
